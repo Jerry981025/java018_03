8feffb99b9c7b169abbc087be78a8ffa63a6cf88
DESKTOP-VBUJM5O\Kulla <kulla9527@gmail.com>
2022-08-16 20:18:07 +0800

    123453

--- src/main/java/test/test.java
+++ src/main/java/test/test.java
@@ -24,5 +24,6 @@ public class test extends HttpServlet {
 		String s = sdf.format(new Date(now));
 		out.println("Hello, World, 大家好 " + s);
 		out.println("JerryTest");
+		out.println("KullaTest");
 	}
 }
