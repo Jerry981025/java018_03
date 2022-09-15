5264ca5ba5c6a4f618cc0a83799a0ea9362a066c
DESKTOP-VBUJM5O\Kulla <kulla9527@gmail.com>
2022-09-15 22:42:07 +0800

    mvc config

---
+++ src/main/java/_config/WebAppConfig.java
@@ -0,0 +1,49 @@
+package _config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("com.web.store")
+public class WebAppConfig implements WebMvcConfigurer {
+	@Bean
+	public ViewResolver internalResouceViewResolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+	
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource rbms = new ResourceBundleMessageSource();
+		rbms.setBasename("messages");
+		return rbms;
+	}
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/css/**")
+				.addResourceLocations("/css/");
+		registry.addResourceHandler("/image/**")
+		.addResourceLocations("/images/");
+	}
+	
+	@Bean
+	public CommonsMultipartResolver multipartResolver() {
+		CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+		resolver.setDefaultEncoding("UTF-8");
+		resolver.setMaxUploadSize(81920000);
+		return resolver;
+	}
+}
