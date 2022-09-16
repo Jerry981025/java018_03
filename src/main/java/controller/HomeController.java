aee72c0f498144005102bf98445338e7e1501416
Jerry <ttps981025@gmail.com>
2022-09-16 22:06:02 +0800

    新增MVC環境method

--- src/main/java/controller/HomeController.java
+++ src/main/java/controller/HomeController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 	
-	@GetMapping("/java018_03/")
+	@GetMapping("/")
 	public String home() {
-		return "123";
+		return "index";
 	}
 	
 }
