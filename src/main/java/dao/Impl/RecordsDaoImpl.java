cf65cfee5be6496af016f06e6304521aa0455731
Cori <huieun0621@gmail.com>
2022-10-01 13:09:33 +0800

    紀錄調整

--- src/main/java/dao/Impl/RecordsDaoImpl.java
+++ src/main/java/dao/Impl/RecordsDaoImpl.java
@@ -4,12 +4,16 @@ import java.util.List;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import dao.RecordsDao;
 import model.OrderBean;
 
 public class RecordsDaoImpl implements RecordsDao {
+	
+	private static Logger log = LoggerFactory.getLogger(RecordsDaoImpl.class);
 	
 	SessionFactory factory;
 	
@@ -21,6 +25,7 @@ public class RecordsDaoImpl implements RecordsDao {
 	
 	@Override
 	public OrderBean findById(Integer oId) {
+		log.info("依照oId編號讀取特定一筆訂單的所有資料之Dao, orderId=" + oId);
 		OrderBean ob = null;
 		Session session = factory.getCurrentSession();
 		ob = session.get(OrderBean.class, oId);
@@ -29,12 +34,14 @@ public class RecordsDaoImpl implements RecordsDao {
 	
 	@Override
 	public List<OrderBean> findByMemberId(Integer mId) {
+		log.info("讀取某位會員所有訂單之Dao: 開始");
 		List<OrderBean> list = null;
 		Session session = factory.getCurrentSession();
 		String hql = "FROM OrderBean ob WHERE ob.memberId = :mid";
 		list = session.createQuery(hql, OrderBean.class)
 					  .setParameter("mid", mId)
 					  .getResultList();
+		log.info("讀取某位會員所有訂單之Dao: 讀取完畢");
 		return list;
 	}
 	
