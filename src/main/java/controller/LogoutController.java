a98cd0164256d59eac76e85f75cda62d2106f693
vip282134@gmail.com <vip282134@gmail.com>
2022-10-04 22:36:55 +0800

    登入和註冊完成後可以跳轉頁面

--- src/main/java/controller/LogoutController.java
+++ src/main/java/controller/LogoutController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.support.SessionStatus;
 
 @RestController
-@RequestMapping("member/logout")
+@RequestMapping("/logout")
 public class LogoutController {
 
 	@GetMapping
